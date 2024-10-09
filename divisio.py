1bb3bb2e9a55c136a8b178bde0f6b9128fab3140
amirashobetbiyah <afathi@instituticaria.cat>
2024-10-09 16:44:49 +0200

    divisio.py

---
+++ divisio.py
@@ -0,0 +1,7 @@
+divident = int(input("Introdueix el divident:"))
+divisor = int(input("Introdueix el divisor:"))
+quocient = divident // divisor
+residu = divident % divisor
+print(f"divisió: {divident} / {divisor}")
+print(f"quocient: {quocient}")
+print(f"residu: {residu}")
