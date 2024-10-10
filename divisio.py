1f96dcfb32a3486f1d2ba21d5771f6259d275c86
amirashobetbiyah <afathi@instituticaria.cat>
2024-10-10 09:07:12 +0200

    Segon Enviament

--- divisio.py
+++ divisio.py
@@ -2,6 +2,6 @@ Divident = int(input("Introdueix el Divident:"))
 Divisor = int(input("Introdueix el Divisor:"))
 Quocient = Divident // Divisor
 Residu = Divident % Divisor
-print(f"Divisió: {Divident} / {Divisor}")
+print(f"Divisió: {Divident}/{Divisor}")
 print(f"Quocient: {Quocient}")
 print(f"Residu: {Residu}")
